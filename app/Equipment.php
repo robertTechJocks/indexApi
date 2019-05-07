137bbc9fb56274ff2a1d2b88c1840b76f82cfd40
Robert Metcalf <rob.metcalf@luminfire.net>
2019-05-06 21:33:22 -0500

    stats json auto decode

--- app/Equipment.php
+++ app/Equipment.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Equipment extends Model
 {
     protected $table = "equipment";
+
+    public function getStatsJsonAttribute($value)
+    {
+        return json_decode($value);
+    }
 }
